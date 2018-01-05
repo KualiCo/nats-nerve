975b5e617b47b87871a13470ded3b997a3e8f119
Thomas A. Valletta <tom@vallettas.com>
2018-01-04 17:07:36 -0700

    Initial commit

---
+++ src/nerve.js
@@ -0,0 +1,298 @@
+/* Copyright © 2016 Kuali, Inc. - All Rights Reserved
+ * You may use and modify this code under the terms of the Kuali, Inc.
+ * Pre-Release License Agreement. You may not distribute it.
+ *
+ * You should have received a copy of the Kuali, Inc. Pre-Release License
+ * Agreement with this file. If not, please write to license@kuali.co.
+ */
+
+const os = require('os')
+const {
+  get,
+  findKey,
+  isFunction,
+  isString,
+  isPlainObject,
+  noop
+} = require('lodash')
+const nats = require('node-nats-streaming')
+const shortid = require('shortid')
+
+const CLIENT_CONNECTION_MAP = {}
+const TEST = process.env.NODE_ENV === 'test'
+const ERRORS = {
+  CLIENT_ID: 'stan: clientID already registered',
+  CONNECTION: 'NatsError: Could not connect to server'
+}
+
+async function getInstance (server, cluster, appName, logger) {
+  const clientId = getClientId(appName)
+  const inst = CLIENT_CONNECTION_MAP[clientId]
+
+  if (inst) {
+    return inst
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    const nerve = new Nerve(server, cluster, clientId, logger)
+    nerve
+      .connect()
+      .then(() => {
+        resolve(nerve)
+      })
+      .catch(err => {
+        logger.error({ err }, 'Failed to connect Nerve')
+        reject(err)
+        delete CLIENT_CONNECTION_MAP[clientId]
+      })
+  })
+  CLIENT_CONNECTION_MAP[clientId] = promise
+  return promise
+}
+
+/* Nats Event Receptor, Validator and Emitter */
+class Nerve {
+  constructor (server, cluster, clientId, logger) {
+    this.connect = this.connect.bind(this)
+    this.reconnect = this.reconnect.bind(this)
+    this.close = this.close.bind(this)
+    this._onConnect = this._onConnect.bind(this)
+    this._onClose = this._onClose.bind(this)
+    this._onError = this._onError.bind(this)
+    this._pub = this._pub.bind(this)
+    this._enqueue = this._enqueue.bind(this)
+    this._dequeue = this._dequeue.bind(this)
+
+    this.id = shortid.generate()
+    this.backlog = []
+    this.notifyOnConnect = []
+    this.notifyOnClose = []
+    this.notifyOnError = []
+    this.server = server || 'nats://127.0.0.1:4222'
+    this.cluster = cluster
+    this.clientId = clientId
+    this.isConnecting = false
+    this.conn = undefined
+    this.requestClose = false
+    this.publisher = {
+      publish: this._enqueue,
+      quit: () => ({ status: 'not ready' })
+    }
+    this.logger = logger
+  }
+
+  connect () {
+    if (this.isConnecting) {
+      return new Promise((resolve, reject) => {
+        this.notifyOnConnect.push(() => {
+          resolve(this.conn)
+        })
+        this.notifyOnClose.push(() => {
+          resolve(this.conn)
+        })
+        this.notifyOnError.push(err => {
+          reject(err)
+        })
+      })
+    }
+    if (this.conn && this.conn.nc && this.conn.nc.connected) {
+      return Promise.resolve(this.conn)
+    }
+    this.isConnecting = true
+    return new Promise((resolve, reject) => {
+      const conn = nats.connect(this.cluster, this.clientId, this.server)
+      conn.on('connect', () => {
+        this._onConnect(conn)
+        resolve(conn)
+      })
+      conn.on('close', () => {
+        resolve(this._onClose(conn))
+      })
+      conn.on('error', err => {
+        this._onError(err, conn)
+        reject(err)
+      })
+    })
+  }
+
+  reconnect () {
+    this.attemptingReconnect = true
+    this.publisher.publish = this._enqueue
+    const startTime = Date.now()
+    return proceedWhen(
+      () => {
+        this.logger.info('Nerve attempting to reconnect...', {
+          ranAt: Date.now() - startTime,
+          connected: get(this.conn, 'nc.connected')
+        })
+        this.connect().catch(noop)
+        return get(this.conn, 'nc.connected')
+      },
+      640000,
+      200
+    ).then(() => {
+      this.logger.info('Nerve reconnected')
+      this.attemptingReconnect = false
+    })
+  }
+
+  close () {
+    return new Promise((resolve, reject) => {
+      this.requestClose = true
+      this.notifyOnClose.push(() => {
+        delete CLIENT_CONNECTION_MAP[this.clientId]
+        resolve(this.conn)
+      })
+      this.notifyOnError.push(err => {
+        reject(err)
+      })
+      if (this.conn) {
+        this.conn.close()
+      } else {
+        resolve()
+      }
+    })
+  }
+
+  subscribe (channel, x, y, z) {
+    const args = [x, y, z]
+    const callback = args.find(arg => isFunction(arg))
+    const group = args.find(arg => isString(arg))
+    const _opts = args.find(arg => isPlainObject(arg)) || {}
+
+    const opts = this.conn.subscriptionOptions()
+    if (_opts.durableName) opts.setDurableName(_opts.durableName)
+    const startOpts = [
+      'startWithLastReceived',
+      'deliverAllAvailable',
+      'startAtSequence',
+      'startTime',
+      'startAtTimeDelta'
+    ]
+    const startOpt = findKey(_opts, (value, key) => {
+      return startOpts.includes(key)
+    })
+
+    if (startOpt) {
+      const fnName = startOpt.charAt(0).toUpperCase() + startOpt.slice(1)
+      opts[`set${fnName}`](_opts[startOpt])
+    } else {
+      opts.setDeliverAllAvailable()
+    }
+    opts.setManualAckMode(_opts.manualAckMode)
+    if (_opts.ackWait !== undefined) opts.setAckWait(_opts.ackWait)
+    if (_opts.maxInFlight !== undefined) opts.setMaxInFlight(_opts.maxInFlight)
+    const subscription = this.conn.subscribe(channel, group, opts)
+    subscription.on('message', callback)
+    subscription.on('error', err => {
+      this.logger.error({ err, channel, x, y, z }, 'Nerve Subscribe Error!')
+    })
+    return subscription
+  }
+
+  isConnected () {
+    return !!this.conn.nc.connected
+  }
+
+  equals (nerve) {
+    return nerve.id === this.id
+  }
+
+  _onConnect (conn) {
+    this.conn = conn
+    this.isConnecting = false
+    this._dequeue()
+    this.publisher.publish = this._pub
+    while (this.notifyOnConnect.length) {
+      this.notifyOnConnect.shift()()
+    }
+  }
+
+  _onClose () {
+    while (this.notifyOnClose.length) {
+      this.notifyOnClose.shift()()
+    }
+    this.publisher.publish = this._enqueue
+    if (this.requestClose) return { closed: true }
+    return this.reconnect()
+  }
+
+  _onError (err) {
+    this.isConnecting = false
+    const errorMessage = err.toString()
+    while (this.notifyOnError.length) {
+      this.notifyOnError.shift()(err)
+    }
+    if (errorMessage === ERRORS.CLIENT_ID) {
+      this.requestClose = true
+      this.logger.error(err)
+    } else if (errorMessage.startsWith(ERRORS.CONNECTION)) {
+      if (!this.attemptingReconnect) {
+        this.reconnect()
+      }
+    } else {
+      this.logger.error(err)
+    }
+  }
+
+  _pub (channel, message) {
+    return new Promise((resolve, reject) => {
+      const msg =
+        typeof message === 'string' ? message : JSON.stringify(message)
+      this.conn.publish(channel, msg, (err, guid) => {
+        if (err) {
+          if (err.message === 'stan: publish ack timeout') {
+            this.logger.warn(
+              { channel, msg },
+              'This message was not acknowledged before the specified timeout.'
+            )
+          } else {
+            reject(err)
+          }
+        } else {
+          resolve(guid)
+        }
+      })
+    })
+  }
+
+  _enqueue (channel, value) {
+    this.backlog.push({ channel, value })
+  }
+
+  _dequeue () {
+    this.backlog.forEach(({ channel, value }) => this._pub(channel, value))
+    this.backlog = []
+  }
+}
+
+function getClientId (appName) {
+  const host = os
+    .hostname()
+    .replace(/\./g, '-')
+    .toLowerCase()
+  return `${appName}_${host}_${TEST ? process.pid : ''}`
+}
+
+function proceedWhen (cb, timeout = 2000, backoff = 10) {
+  return new Promise((resolve, reject) => {
+    let _interval
+    let _timeout = setTimeout(() => {
+      clearTimeout(_interval)
+      reject(new Error('Timout error waiting for event'))
+    }, timeout)
+    function exponentialBackoff (_backoff) {
+      _interval = setTimeout(() => {
+        if (cb()) {
+          clearTimeout(_timeout)
+          resolve(true)
+        } else {
+          exponentialBackoff(_backoff * 2)
+        }
+      }, _backoff)
+    }
+    exponentialBackoff(backoff)
+  })
+}
+
+module.exports = { getInstance, Nerve, getClientId, proceedWhen }
---
+++ src/nerve.spec.js
@@ -0,0 +1,266 @@
+/* Copyright © 2016 Kuali, Inc. - All Rights Reserved
+ * You may use and modify this code under the terms of the Kuali, Inc.
+ * Pre-Release License Agreement. You may not distribute it.
+ *
+ * You should have received a copy of the Kuali, Inc. Pre-Release License
+ * Agreement with this file. If not, please write to license@kuali.co.
+ */
+const assert = require('assert')
+const shortid = require('shortid')
+const Promise = require('bluebird')
+const { getInstance: Nerve, getClientId, proceedWhen } = require('./nerve')
+
+describe('Nerve', () => {
+  const appName = 'nats-nerve'
+  const server = 'nats://127.0.0.1:4222'
+  const cluster = 'test-cluster'
+
+  function getAppName () {
+    return `${shortid.generate()}_${appName}`
+  }
+
+  let logger
+  beforeEach(() => {
+    logger = {
+      trace: console.log, // eslint-disable-line
+      debug: console.log, // eslint-disable-line
+      info: console.log, // eslint-disable-line
+      warn: console.log, // eslint-disable-line
+      error: console.log, // eslint-disable-line
+      fatal: console.log // eslint-disable-line
+    }
+  })
+
+  it('can construct', async () => {
+    const appName = getAppName()
+    const nerve = await Nerve(server, cluster, appName, logger)
+    assert.equal(nerve.cluster, cluster)
+    assert.equal(nerve.clientId, getClientId(appName))
+    assert.equal(nerve.isConnecting, false)
+    assert.equal(nerve.isConnected(), true)
+    await nerve.close()
+  })
+
+  it('if the connection is unexpectedly closed it reconnects', async () => {
+    const nerve = await Nerve(server, cluster, getAppName(), logger)
+    nerve.conn.close()
+    let isReconnected = false
+    nerve.notifyOnConnect.push(() => {
+      isReconnected = true
+    })
+    await proceedWhen(() => isReconnected)
+    await nerve.close()
+  })
+
+  it('defaults the server if it is undefined', async () => {
+    const nerve = await Nerve(undefined, cluster, getAppName(), logger)
+    assert.equal(nerve.isConnected(), true)
+    await nerve.close()
+  })
+
+  it('errors if the server is not up', async () => {
+    logger.error = () => {
+      /* do nothing */
+    }
+    try {
+      await Nerve('nats://127.0.0.1:4333', cluster, getAppName(), logger)
+      assert.fail()
+    } catch (err) {
+      assert.ok(err)
+    }
+  })
+
+  it('returns the connection if it already has been created', async () => {
+    const nerve = await Nerve(server, cluster, getAppName(), logger)
+    const conn1 = await nerve.connect()
+    const conn2 = await nerve.connect()
+    assert.equal(conn1, conn2)
+    await nerve.close()
+  })
+
+  it('reuses an instance when connecting with the same client id', async () => {
+    const appName = getAppName()
+    const nerve1 = await Nerve(server, cluster, appName, logger)
+    const nerve2 = await Nerve(server, cluster, appName, logger)
+    assert.ok(nerve1.equals(nerve2))
+    await nerve1.close()
+  })
+
+  it('can publish and subscribe', async () => {
+    const channel = new Date().toISOString()
+    const nerve = await Nerve(server, cluster, getAppName(), logger)
+    await nerve.connect()
+    let received = false
+    const subscription = nerve.subscribe(channel, msg => {
+      received = msg.getData() === 'hi'
+    })
+    await nerve.publisher.publish(channel, 'hi')
+    await proceedWhen(() => received)
+    await unsubscribe(subscription)
+    await nerve.close()
+  })
+
+  it('can publish and subscribe objects', async () => {
+    const channel = new Date().toISOString()
+    const nerve = await Nerve(server, cluster, getAppName(), logger)
+    await nerve.connect()
+    let received = false
+    const subscription = nerve.subscribe(channel, msg => {
+      const data = JSON.parse(msg.getData())
+      received = data.id === 1 && data.payload === 'hi'
+    })
+    try {
+      await nerve.publisher.publish(channel, { id: 1, payload: 'hi' })
+    } catch (err) {
+      console.log('published message was never acknowledged :3:')
+    }
+    await proceedWhen(() => received)
+    await unsubscribe(subscription)
+    await nerve.close()
+  })
+
+  it('can reset a durable subscription by unsubscribing', async () => {
+    const channel = shortid.generate()
+    const nerve = await Nerve(server, cluster, getAppName(), logger)
+    await nerve.connect()
+    const received = [0, 0, 0, 0, 0]
+    const opts = { durableName: shortid.generate() }
+    await nerve.publisher.publish(channel, { id: 0, payload: 'hi, 0!' })
+    const subscription1 = nerve.subscribe(channel, opts, msg => {
+      const data = JSON.parse(msg.getData())
+      received[data.id]++
+    })
+    await nerve.publisher.publish(channel, { id: 1, payload: 'hi, 1!' })
+    await nerve.publisher.publish(channel, { id: 2, payload: 'hi, 2!' })
+    await proceedWhen(() => !!received[2])
+
+    /**
+     * This is the magic here. If the subscription is unsubscribed, then
+     * subscribing to the same channel as the same client with the same
+     * durableName does not continue the subscription, it starts it over.
+     */
+    await unsubscribe(subscription1)
+
+    await nerve.publisher.publish(channel, { id: 3, payload: 'hi, 3!' })
+    const subscription2 = nerve.subscribe(channel, opts, msg => {
+      const data = JSON.parse(msg.getData())
+      received[data.id]++
+    })
+    await nerve.publisher.publish(channel, { id: 4, payload: 'hi, 4!' })
+    await proceedWhen(() => !!received[4])
+    await unsubscribe(subscription2)
+    await nerve.close()
+    expect(received).toEqual([2, 2, 2, 1, 1])
+  })
+
+  it('can continue a durable subscription after closing', async () => {
+    const channel = shortid.generate()
+    const nerve = await Nerve(server, cluster, getAppName(), logger)
+    await nerve.connect()
+    const received = [0, 0, 0, 0, 0]
+    const opts = { durableName: shortid.generate() }
+    await nerve.publisher.publish(channel, { id: 0, payload: 'hi, 0!' })
+    const subscription1 = nerve.subscribe(channel, opts, msg => {
+      const data = JSON.parse(msg.getData())
+      received[data.id]++
+    })
+    await nerve.publisher.publish(channel, { id: 1, payload: 'hi, 1!' })
+    await nerve.publisher.publish(channel, { id: 2, payload: 'hi, 2!' })
+    await proceedWhen(() => !!received[2])
+
+    /**
+     * This is the magic here. If the subscription is closed but not
+     * unsubscribed, then subscribing to the same channel as the same client
+     * with the same durableName does continue the subscription. It just takes
+     * over where it left off.
+     */
+    await subscription1.close()
+
+    await nerve.publisher.publish(channel, { id: 3, payload: 'hi, 3!' })
+    const subscription2 = nerve.subscribe(channel, opts, msg => {
+      const data = JSON.parse(msg.getData())
+      received[data.id]++
+    })
+    await nerve.publisher.publish(channel, { id: 4, payload: 'hi, 4!' })
+    await proceedWhen(() => !!received[4])
+    await unsubscribe(subscription2)
+    await nerve.close()
+    expect(received).toEqual([1, 1, 1, 1, 1])
+  })
+
+  it('can handle disconnects', async () => {
+    const channel = new Date().toISOString()
+    const durableName = `random-${+new Date()}`
+    const nerve = await Nerve(server, cluster, getAppName(), logger)
+
+    nerve.publisher.publish(channel, '0')
+    await nerve.connect()
+    nerve.publisher.publish(channel, '1')
+    await nerve.close()
+    nerve.publisher.publish(channel, '2')
+    await nerve.connect()
+    nerve.publisher.publish(channel, '3')
+
+    const received = [false, false, false, false]
+    const subscription = nerve.subscribe(channel, { durableName }, msg => {
+      const value = +msg.getData()
+      received[value] = !received[value]
+    })
+
+    await proceedWhen(
+      () => received[0] && received[1] && received[2] && received[3]
+    )
+    await unsubscribe(subscription)
+    await nerve.close()
+  })
+
+  it('attempts reconnect if unable to connect', async () => {
+    let done = false
+    logger.info = jest.fn()
+      .mockImplementationOnce(() => 'one')
+      .mockImplementationOnce(() => {
+        done = true
+        return 'two'
+      })
+    class NatsError extends Error {
+      constructor (message) {
+        super(message)
+        this.name = 'NatsError'
+      }
+    }
+
+    try {
+      const nerve = await Nerve(server, cluster, getAppName(), logger)
+      nerve.conn.emit('error', new NatsError('Could not connect to server'))
+    } catch (err) {
+      /* no nothing */
+    }
+    await proceedWhen(() => done)
+    expect(logger.info.mock.calls[0][0])
+      .toBe('Nerve attempting to reconnect...')
+    expect(logger.info.mock.calls[1][0])
+      .toBe('Nerve reconnected')
+  })
+
+  it('proceedWhen times out', async () => {
+    const nerve = await Nerve(server, cluster, getAppName(), logger)
+    let timeoutError = false
+    try {
+      await proceedWhen(() => false, 5, 1)
+    } catch (err) {
+      timeoutError = true
+    }
+    await proceedWhen(() => timeoutError)
+    await nerve.connect()
+    await nerve.close()
+  })
+})
+
+function unsubscribe (subscription) {
+  return new Promise((resolve, reject) => {
+    subscription.unsubscribe()
+    subscription.on('unsubscribed', () => {
+      resolve()
+    })
+  })
+}
